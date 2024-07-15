1698162f0288bd7e060371999c36419bb43e5a84
Azarow <thibault.pointurier.pro@gmail.com>
2024-07-15 16:34:17 +0200

    first commit

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-export * from './src/driver.js'
+import { MicrosoftDriver } from './src/microsoft.js'
+import type { MicrosoftDriverConfig } from './src/types/main.js'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export function microsoft(config: MicrosoftDriverConfig) {
+  return (ctx: HttpContext) => new MicrosoftDriver(ctx, config)
+}
---
+++ src/types/main.ts
@@ -0,0 +1,24 @@
+import type {
+  AllyDriverContract,
+  LiteralStringUnion,
+  Oauth2DriverConfig,
+} from '@adonisjs/ally/types'
+
+export interface MicrosoftDriverContract extends AllyDriverContract<MicrosoftToken, MicrosoftScopes> {
+    version: 'oauth2'
+}
+
+export type MicrosoftDriverConfig = Oauth2DriverConfig & {
+  scopes?: LiteralStringUnion<MicrosoftScopes>[]
+}
+
+export type MicrosoftToken = {
+    expiresAt: Date
+    expiresIn: number
+    refreshToken: string
+    scope: string[]
+    token: string
+    type: 'bearer'
+}
+
+export type MicrosoftScopes = 'openid'
